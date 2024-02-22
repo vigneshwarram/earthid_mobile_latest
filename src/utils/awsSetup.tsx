099cbff49d676e1d42cd5482f604fb8d2fc2ec70
Artur Stokkeland <arturst95@gmail.com>
2024-02-22 23:18:38 +0000

    remove secret keys

--- src/utils/awsSetup.tsx
+++ src/utils/awsSetup.tsx
@@ -1,12 +1,13 @@
 
 import AWS from 'aws-sdk';
 import RNFetchBlob from 'rn-fetch-blob';
+import { ACCESS_KEY, SECRET_KEY } from '@env';
 
 //AWS S3 bucket
 const bucketName = 'idv-sessions'
 // AWS credentials
-const accessKey = 'AKIAU4JK3LMEAKCXLXWE';
-const secretKey = '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';
+const accessKey = ACCESS_KEY;
+const secretKey = SECRET_KEY;
 
 AWS.config.update({
   accessKeyId: accessKey,
